e20c1fb7690f4f2d1898c3ed4b34d86ab3343837
Cameron Givler <cameron@aqueti.com>
2016-09-25 16:13:26 -0400

    added /media/data1 mount

--- TegraFreshInstall.sh
+++ TegraFreshInstall.sh
@@ -6,6 +6,9 @@
 
 ./shared.sh
 
+# Should change /dev/sda1 to UUID="<uuid>"
+echo '/dev/sda1 /media/data1 ext4 user,noatime 0 0' | sudo tee -a /etc/fstab
+
 # Create softlinks to clang 3.8 so that cmake finds it FOR TEGRA ONLY
 sudo apt install clang-3.6
 sudo ln -s /usr/bin/clang-3.6 /usr/bin/clang
