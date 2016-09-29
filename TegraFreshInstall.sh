bc8c45827a97853f1f7f0aeadc8b246ac5b3ef7f
Cameron Givler <cameron@aqueti.com>
2016-09-29 21:20:06 +0000

    auto mount and install clang

--- TegraFreshInstall.sh
+++ TegraFreshInstall.sh
@@ -4,13 +4,14 @@
 # sudo adduser --uid 1738 cameron
 # sudo adduser cameron sudo
 
-./shared.sh
+sudo sh shared.sh
 
 # Should change /dev/sda1 to UUID="<uuid>"
 echo '/dev/sda1 /media/data1 ext4 user,noatime 0 0' | sudo tee -a /etc/fstab
+mount /media/data1
 
 # Create softlinks to clang 3.8 so that cmake finds it FOR TEGRA ONLY
-sudo apt install clang-3.6
+sudo apt install -y clang-3.6
 sudo ln -s /usr/bin/clang-3.6 /usr/bin/clang
 sudo ln -s /usr/bin/clang++-3.6 /usr/bin/clang++
 
