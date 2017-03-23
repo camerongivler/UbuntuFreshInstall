72b29db0974e4efa44de4fc3fc15bef6604f87c2
Cameron Givler <cameron@aqueti.com>
2017-03-23 14:36:48 -0400

    no separate tegra clang

--- TegraFreshInstall.sh
+++ TegraFreshInstall.sh
@@ -7,11 +7,6 @@
 ./shared.sh
 ./once.sh
 
-# Create softlinks to clang 3.8 so that cmake finds it FOR TEGRA ONLY
-sudo apt install -y clang
-sudo ln -s /usr/bin/clang /usr/bin/clang
-sudo ln -s /usr/bin/clang++ /usr/bin/clang++
-
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib' >> /home/cameron/.bashrc
 
 echo #newline
